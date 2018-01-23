9a8802ca6432583e80a8d21cdea3a92af9a27961
ackzell <ackzell@gmail.com>
2018-01-22 23:40:42 -0600

    #1 Translating Premade Themes.

--- lang/es-MX/vuetify/PreMadeThemes.js
+++ lang/es-MX/vuetify/PreMadeThemes.js
@@ -1,30 +1,30 @@
 export default {
-  header: 'Free and Premium themes',
-  headerText: 'Vuetify offers both **free** and **premium** pre-made themes designed to get you started in a flash. Free themes are available to install through vue-cli or you can simply download the source.',
-  free: 'Free',
+  header: 'Temas gratuitos y premium',
+  headerText: 'Vuetify ofrece temas prefabricados **gratuitos** y **premium** para que comiences en un pestañeo. Los temas gratuitos están disponibles par ser instalados a través del vue-cli o puedes simplemente descargar el código fuente.',
+  free: 'Gratis',
   demo: 'Demo',
-  comingSoon: 'coming soon',
-  sourceCode: 'source code',
+  comingSoon: 'próximamente',
+  sourceCode: 'código fuente',
   templates: [
     {
       screenshot: '/static/doc-images/starter/vuetify-premium.jpg',
       title: 'Startup',
       tag: 'Premium',
-      description: 'Showcase your Company or personal portfolio with this beautifully hand-crafted Material design template.',
+      description: 'Presenta tu Empresa o tu portafolio personal con esta plantilla de Material Design bellamente hecha a mano.',
       price: '25$',
       demoUrl: ''
     },
     {
       screenshot: '/static/doc-images/starter/vuetify-parallax-starter.png',
       title: 'Parallax',
-      description:'This beautiful single page parallax is a great home page for any application.',
+      description:'Esta bella _single page_ con parallax es una gran página de inicio para cualquier aplicación.',
       sourceUrl: 'vuetifyjs/parallax-starter',
       demoUrl: '/themes/parallax-starter'
     },
     {
       screenshot: '/static/doc-images/starter/vuetify-blog-starter.png',
       title: 'Blog',
-      description:'A simple template that features a clean interface for creating a blog or blog-like application.',
+      description:'Una plantilla simple que lleva una interfaz sencilla para crear un blog o una aplicación similar.',
       sourceUrl: 'vuetifyjs/blog-starter',
       demoUrl: '/themes/blog-starter'
     }
