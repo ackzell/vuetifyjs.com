46bcd565709dfdb30d3f697bae212a964e87a732
ackzell <ackzell@gmail.com>
2018-01-27 15:49:40 -0600

    #2 Translating SponsorsAndBackers.js

--- lang/es-MX/getting-started/SponsorsAndBackers.js
+++ lang/es-MX/getting-started/SponsorsAndBackers.js
@@ -1,13 +1,13 @@
 export default {
-  header: `Support Vuetify's development`,
-  headerText: 'Vuetify is an open source project under the MIT license. It is and will always be free to use. However, the amount of effort it takes to maintain the core framework and all supplementing packages is quite considerable. With the goal to continue development of additional functionality even after the completion of Material Design spec, you can help by <a href="https://www.patreon.com/vuetify" target="_blank" rel="noreferrer">backing Vuetify on Patreon</a>.',
-  backersHeader: 'Patreon backers:',
-  backersSubHeader: 'Diamond ($500/mo)',
-  backersSubHeader2: 'Palladium ($250/mo)',
-  backersSubHeader3: 'Platinum ($100/mo)',
-  backersSubHeader4: 'Gold ($50/mo)',
-  affiliatesHeader: 'Affiliates:',
-  sponsorsHeader: 'Service sponsors:',
-  questionHeader: 'Have additional questions?',
-  questionButton: 'Contact Us'
+  header: `Apoya al desarrollo de Vuetify`,
+  headerText: 'Vuetify es un proyecto open source bajo la licencia MIT. Es y siempre será gratuito para usarse. Sin embargo, la cantidad de esfuerzo que toma mantener el core del framework y todos los paquetes complementarios es bastante considerable. Con la meta de continuar el desarrollo de funcionalidad adicional aún después de completar el spec de Material Design, puedes ayudar <a href="https://www.patreon.com/vuetify" target="_blank" rel="noreferrer">aportando a Vuetify en Patreon</a>.',
+  backersHeader: 'Patrocinadores en Patreon:',
+  backersSubHeader: 'Diamond (USD $500/mo)',
+  backersSubHeader2: 'Palladium (USD $250/mo)',
+  backersSubHeader3: 'Platinum (USD $100/mo)',
+  backersSubHeader4: 'Gold (USD $50/mo)',
+  affiliatesHeader: 'Afiliados:',
+  sponsorsHeader: 'Patrocinadores de servicios:',
+  questionHeader: '¿Tienes más preguntas?',
+  questionButton: 'Contáctanos'
 }
