58b07e6e386dd21c3ff8e9e003ca26b1e6a83549
ackzell <ackzell@gmail.com>
2018-02-01 19:48:04 -0600

    #3 Translating ServerSideRendering.js

--- lang/es-MX/guides/ServerSideRendering.js
+++ lang/es-MX/guides/ServerSideRendering.js
@@ -1,51 +1,51 @@
 export default {
-  header: 'Overview',
-  headerText: 'This overview is designed to help you become fluent in **Vuetify\'s** Vue SSR Template. If you have not already installed the <code>webpack-ssr</code> template, please do so <router-link to="/vuetify/quick-start">in the Quick start</router-link>.',
-  structureHeader: 'Structure',
-  structureText1: 'This Vue CLI template was designed for performance, seo optimization and usability. This template is configured out of the box for css, sass and stylus pre-processors.',
-  structureText2: 'The **Build** folder contains all of the webpack specific build configurations for your project. **Src** is where all the development project files reside. Notice that the Webpack-SSR template is out-of-the-box configured to use [Vue Router](https://router.vuejs.org/en/), [Vuex](https://vuex.vuejs.org/en/intro.html), and the [Vue Server Renderer](https://vuejs.org/v2/guide/ssr.html). This will allow you to make simple or complex applications that are not only fast/efficient, but **SEO** friendly.',
-  structureSubHeader: 'Folder Structure',
-  applicationHeader: 'Application',
-  applicationText1: 'Navigate to the **src** folder and open up <code>App.vue</code>. Vuetify is a semantic-focused framework. The code you write should be easy to remember, and easy to manage. To do this, one of the main components of Vuetify is <code>v-app</code>. This component allows you to define your application layout. This is used in conjunction with <code>v-toolbar</code>, <code>v-navigation-drawer</code>, and <code>v-footer</code>.',
-  applicationText2: 'The markup below tells the application that you have a <code>toolbar</code> and <code>footer</code>. Once defined, the content area will be resized to accommodate. For more information on layouts, navigate to the <router-link to="/layouts/pre-defined">Layouts</router-link> section.',
-  applicationText3: 'Place all statically served assets in <code>static</code>. This can be accessed by referencing <code>/static/picture.png</code>, for example.',
-  applicationText4: 'The components folder is where your custom components should be placed. Edit the <code>components/_index.js</code> file to have these files automatically bootstrapped for you.',
-  applicationSubHeader1: 'Static assets',
-  applicationSubHeader2: 'Components',
+  header: 'Información general',
+  headerText: 'Esta información general está diseñada para ayudarte a dominar la plantilla de SSR (Renderizado del lado del Servidor) **Vuetify**. Si no has instalado la plantilla <code>webpack-ssr</code>, por favor házlo <router-link to="/vuetify/quick-start">en el Inicio Rápido</router-link>.',
+  structureHeader: 'Estructura',
+  structureText1: 'Esta plantilla de Vue CLI fue diseñada pensando en el desempeño, SEO y usabilidad. Esta plantilla está configurada de inicio para CSS, Sass y Stylus.',
+  structureText2: 'La carpeta **build** contiene todas las configuraciones específicas de webpack para tu proyecto. *src* es donde residen todos los archivos de desarrollo del proyecto. Observa que la plantilla de Webpack-SSR está configurada desde-el-inicio para usar [Vue Router](https://router.vuejs.org/en/), [Vuex](https://vuex.vuejs.org/en/intro.html), y el [Vue Server Renderer](https://vuejs.org/v2/guide/ssr.html). Esto te permitirá crear aplicaciones simples o complejas que no sólo son más rápidas/eficientes, pero también SEO-amigables',
+  structureSubHeader: 'Estructura de Carpetas',
+  applicationHeader: 'Applicación',
+  applicationText1: 'Ve a la carpeta **src** y abre <code>App.vue</code>. Vuetify es un framework enfocado en la semántica. El código que escribas debe ser fácil de recordar, y fácil de manejar. Para lograrlo, uno de los componentes de Vuetify es <code>v-app</code>. Este componente permite que definas la distribución (layout) de tu aplicación. Se usa en conjunto con <code>v-toolbar</code>, <code>v-navigation-drawer</code>, y <code>v-footer</code>.',
+  applicationText2: 'El markup a continuación le dice a la aplicación que tienes una <code>toolbar</code> y un <code>footer</code>. Una vez definidos, el área de contenido cambiará de tamaño para ajustarse. Para más información acerca de los layouts, navega a la sección <router-link to="/layouts/pre-defined">Layouts</router-link>.',
+  applicationText3: 'Coloca todos los assets servidos de forma estática en <code>static</code>. Esto puede ser accedido si haces referencia, por ejemplo a <code>/static/picture.png</code>. ',
+  applicationText4: 'El folder de componentes es donde tus componentes personalizados se deben colocar. Edita el archivo <code>components/_index.js</code> para que estos archivos sean cargados por tí automáticamente.',
+  applicationSubHeader1: 'Assets estáticos',
+  applicationSubHeader2: 'Componentes',
   routingHeader: 'Routing',
-  routingText1: 'The Webpack-SSR template uses the official Vue Router for controlling application flow. Located in <code>/src/router/index.js</code>, all of your application routes and route logic will be defined here. Inside you will also see a function that allows you to setup dynamically imported pages. This is not required, however, it will help break up your application and only load views that are needed, clearing up space in your eventual bundle output.',
-  routingText2: 'These routes can be accessed by creating a link to the specified path, or by using Vue Router\'s `<router-link>` component. For more information, review the official Vue Router [documentation](https://router.vuejs.org/en/). The default function assumes you are placing views in the <kbd>/pages</kbd> folder containing **View** on the end of the file. Example <code>HomeView.vue</code>.',
-  stateControlHeader: 'State control',
-  stateControlText1: 'State control is managed by the official Vuex library. This Vue plugin follows Facebook\'s Reflux design pattern. Navigate to <code>/store/index.js</code>. By default, Vuex is setup to prefetch data for the store before your page is initially rendered. To hook into this functionality, create a <code>preFetch</code> method on your view component.',
-  stateControlText2: 'This is useful for bootstrapping your application so that any necessary data is available before the initial render.',
-  stateControlText3: 'For more information on State Control and Vuex, view the official [documentation](https://vuex.vuejs.org/en/intro.html).',
-  stateControlText4: 'Vuetify integrates into Vuex to allow for easy debugging and access to stored properties. You can also use watchers to hook into store states and react when they change. More information can be found [here](https://vuejs.org/v2/guide/reactivity.html).',
+  routingText1: 'La plantilla Webpack-SSR utiliza el Vue Router oficial para controlar el flujo de la applicación. Ubicado en <code>/src/router/index.js</code>, todas tus rutas de la aplicación y la lógica de las mismas serán definidas aquí. Dentro también verás una función que te permite configurar páginas importadas dinámicamente. Aunque no es un requerimiento, te permitirá fragmentar tu aplicación y sólo cargar las vistas que son necesarias, liberando espacio en el paquete que eventualmente vas a producir.',
+  routingText2: 'Estas rutas pueden ser accesadas creando un link al path especificado en ellas, o usando el componente <router-link> de Vue Router. Para más información puedes revisar la [documentación](https://router.vuejs.org/en/) oficial de Vue Router. La función default asume que estás colocando tus vistas en la carpeta <kbd>/pages</kbd> y que contienen **View** al final del archivo. Ejemplo: <code>HomeView.vue</code>.',
+  stateControlHeader: 'Control del Estado (state control)',
+  stateControlText1: 'El control del estado de la aplicación se administra por la librería oficial Vuex. Este plugin de Vue sigue el patrón de diseño Reflux de Facebook. Navega a <code>/store/index.js</code>. Por default, Vuex está configurado para precargar los datos del store antes de que tu pagina sea renderizada por primera vez. Para aprovechar esta funcionalidad, crea un método <code>preFetch</code> en tu componente.',
+  stateControlText2: 'Esto es útil para levantar tu aplicación y que los datos necesarios estén disponibles antes del render inicial.',
+  stateControlText3: 'Para más información acerca del Control del Estado y Vuex, ve a la [documentaciónn](https://vuex.vuejs.org/en/intro.html) oficial.',
+  stateControlText4: 'Vuetify se integra dentro de Vuex para permitirte depurar fácilmente y tener acceso a las propiedades almacenadas. También puedes utilizar watchers para "conectarte" a los estados del store y reaccionar cuando éstos cambien. Puedes encontrar más información [aquí](https://vuejs.org/v2/guide/reactivity.html).',
   metaDataHeader: 'Meta data',
-  metaDataText: 'In order to ensure that page specific meta data is viewable when your pages are crawled, you can define your applications page specific information in <code>router/meta.json</code>. This will allow you to set page title, description and keywords for every route. This will automatically be applied for you on page load and change.',
+  metaDataText: 'Para asegurar que meta data específica de la página es visible cuando tus páginas son rastreadas por agentes de búsqueda (crawlers), puedes definir información específica de la aplicación en <code>router/meta.json</code>. Esto te permitirá definir el título de la página, la descripción y palabras clave para cada ruta. Esto será aplicado automáticamente por tí cuando la página cargue y cuando cambie.',
   redirectsHeader: 'Redirects',
-  redirectsText: 'As applications evolve, routes change. If you want to maintain the authority already garnered from bots that index your page, it is best to assign a 301 redirect to the new location. Located in <code>router/301.json</code> you can define 301 redirects for your pages which will automatically be handled for you on page load.',
-  webAppSupportHeader: 'Web App support',
-  webAppSupportText: 'Vuetify SSR has support for native Web Applications on smart phones. Also known as progressive web apps, your websites can be saved on the homescreen of a device, allowing it to be usable offline and receive push notifications. For more information on Web App Manifest\'s, navigate to the [Mozilla Developer Network](https://developer.mozilla.org/en-US/docs/Web/Manifest). To see a live example, add the Vuetify documentation on your mobile device\'s homescreen.',
+  redirectsText: 'Cuando las aplicaciones van evolucionando, las rutas cambian. Si quieres mantener la autoridad que has adquirido de los bots que indexan tu página, es mejor que asiges un 301 redirect a la nueva ubicación. En <code>router/301.json</code> puedes definir los redirects (301) para tus páginas que automáticamente serán redirigidas cuando cargue cada página.',
+  webAppSupportHeader: 'Soporte para Web App',
+  webAppSupportText: 'Vuetify SSR tiene soporte para Aplicaciones Web Nativas en smartphones. También conocidas como _progressive web apps_, tus sitios web pueden ser guardados en la pantalla home del dispositivo, permitiéndote usarlos aún sin conexión y recibir push notifications. Para mayor información acerca del Web App Manifest, ve a [Mozilla Developer Network](https://developer.mozilla.org/en-US/docs/Web/Manifest). Para ver un ejemplo en vivo, agrega la documentación de Vuetify a la pantalla de home en tu dispositivo móvil.',
   toc: [
     {
-      text: 'Overview',
-      href: 'introduction'
+      text: 'Información General',
+      href: 'introduccion'
     },
     {
-      text: 'Structure',
-      href: 'structure'
+      text: 'Estructura',
+      href: 'estructura'
     },
     {
-      text: 'Application',
-      href: 'application'
+      text: 'Aplicación',
+      href: 'aplicación'
     },
     {
       text: 'Routing',
       href: 'routing'
     },
     {
-      text: 'State control',
-      href: 'state-control'
+      text: 'Control del Estado (state control)',
+      href: 'control-del-estado'
     },
     {
       text: 'Meta data',
@@ -56,8 +56,8 @@ export default {
       href: 'redirects'
     },
     {
-      text: 'Web App support',
-      href: 'web-app-support'
+      text: 'Soporte para Web App',
+      href: 'soporte-para-web-app'
     }
   ]
 }
