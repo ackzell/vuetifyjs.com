6c4fb7a5f5df1f5bdfa72ab294beb1399330b513
ackzell <ackzell@gmail.com>
2018-02-06 23:22:03 -0600

    #5 Translating Content.js

--- lang/es-MX/style/Content.js
+++ lang/es-MX/style/Content.js
@@ -1,12 +1,12 @@
 export default {
-  header: 'Content',
-  headerText: 'Vuetify has custom styling for multiple standard elements.',
-  blockquoteHeader: 'Block quote',
-  paragraphsHeader: 'Paragraphs',
-  codeHeader: 'Code',
-  codeText: 'Example of an inline `<code>` element.',
+  header: 'Contenido',
+  headerText: 'Vuetify tiene personalización de estilos para múltiples elementos estándar.',
+  blockquoteHeader: 'Citas',
+  paragraphsHeader: 'Párrafos',
+  codeHeader: 'Código',
+  codeText: 'Ejemplo de un elemento `<code>`.',
   variablesHeader: 'Variables',
   variablesText: '<var>v</var> = <var>u</var> * <var>e</var>',
   userInputHeader: 'User input',
-  userInputText: 'To install Vuetify, type <kbd>npm install vuetify</kbd> into your console. Once complete, type <kbd>cd `<project name>`</kbd> and run <kbd>npm install</kbd>'
+  userInputText: 'Para instalar Vuetify, escribe <kbd>npm install vuetify</kbd> en tu terminal. Una vez completado, escribe <kbd>cd `<nombre del proyecto>`</kbd> y ejecuta <kbd>npm install</kbd>'
 }
