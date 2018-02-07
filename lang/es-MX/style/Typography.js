c7c7de862240bc5c0a881b5d9babddafb4f295c3
ackzell <ackzell@gmail.com>
2018-02-07 00:07:21 -0600

    #5 Translating Typography.js

--- lang/es-MX/style/Typography.js
+++ lang/es-MX/style/Typography.js
@@ -1,5 +1,5 @@
 export default {
-  header: 'Typography',
-  headerText: 'The typography of an application is just as important as its functionality. Vuetify.js uses the Material Design spec Roboto Font. Each heading size also has a corresponding helper class to style other elements the same.',
-  examplesHeader: 'Examples'
+  header: 'Tipografía',
+  headerText: 'La tipografía de una aplicación est tan importante como su funcionalidad. Vuetify.js utiliza la fuente del spec de Material Design: Roboto Font. Cada tamaño de encabezado también tiene una clase auxiliar para darle el mismo estilo a otros elementos.',
+  examplesHeader: 'Ejemplos'
 }
