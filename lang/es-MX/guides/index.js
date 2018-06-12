6e4096e5688b664f083ba61a17472f0b1940de15
ackzell <ackzell@gmail.com>
2018-06-12 00:08:01 -0500

    #12 Adding all files in json format

---
+++ lang/es-MX/guides/index.js
@@ -0,0 +1,7 @@
+import VueCli3 from './VueCli3'
+import ServerSideRendering from './ServerSideRendering'
+import ALaCarte from './ALaCarte'
+
+export default {
+  VueCli3, ServerSideRendering, ALaCarte
+}
